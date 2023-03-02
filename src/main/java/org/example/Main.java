7bdb7a950c8b315dc4ffd6650a4dbfcdeedffaee
TutukovDA <126241028+TutukovDA@users.noreply.github.com>
2023-03-02 22:39:18 +0400

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Main {
+       public static void main(String[] args) {
+        int x = 99;
+                        while (x>0) {
+                                System.out.println(x + " Butilok stoyalo na stole");
+                                System.out.println("Vozmi odnu butilku i pusti po krugu");
+                                x = x - 1;
+                                System.out.println(x + " Butilok ostanetsa na stole");
+                            }
+           System.out.println("Vot i zakonchilos pivo, poydu v magaz");
+                                    }
+                    }
